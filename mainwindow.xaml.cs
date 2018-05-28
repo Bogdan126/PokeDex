803958d8c86ad73718c9e3cd99299c2996b845d5
Jakub Bogdański <jakub.bogdanski01@student.wat.edu.pl>
2018-05-28 21:18:52 +0200

    Wersja 0.1 (Wstępnie layout, w nastepnej baza danych i layout kolejnych okienek)

---
+++ mainwindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ForFun
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Nowy_Pok_Click(object sender, RoutedEventArgs e)
+        {
+            Nowy_Pok nowy_pok = new Nowy_Pok();
+            nowy_pok.Show();
+        }
+
+        private void Nowy_Pok_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+            Edycja_Poka edycja = new Edycja_Poka();
+            edycja.Show();
+        }
+
+        private void Ustawienia_Click(object sender, RoutedEventArgs e)
+        {
+            Ustawienia ustawienia = new Ustawienia();
+            ustawienia.Show();
+        }
+    }
+}
